d073b87ed22b749998773e15ea2ac9e652d07e0e
natalya.bogdanova <nata-mini@mail.ru>
2019-09-12 14:53:08 +0300

    Changed query at user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
 
   def test_list(level)
-    self.class.joins('INNER JOIN user_tests ON user_tests.user_id = users.id INNER JOIN tests ON tests.id = user_tests.test_id')
-        .where('tests.level = :level AND users.id = :id', level: level, id: self.id)
+
+    Test.joins('INNER JOIN user_tests ON tests.id = user_tests.test_id')
+        .where('tests.level = :level AND user_tests.id = :id', level: level, id: self.id)
         .pluck('tests.title')
   end
 end
