37e60b3677f80ce68d418299f607302016332b50
natalya.bogdanova <nata-mini@mail.ru>
2019-09-14 17:30:32 +0300

    Changed dependent:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :tests_users, dependent: :destroy
   has_many :tests, through: :tests_users, dependent: :destroy
-  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+  has_many :created_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :nullify
 
   def test_list(level)
     Test.joins(:users_tests)
