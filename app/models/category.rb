e3072419188855627fbfa7989f106d949eb99de4
Natalie <ubuntu@Ubuntu.40GB>
2019-09-18 23:19:14 +0930

    changes after review

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   has_many :tests, dependent: :nullify
 
-  validates :title, presence: true, uniqueness: true
+  default_scope { order(title: :asc) }
 
-  default_scope { order(title: :desc) }
+  validates :title, presence: true, uniqueness: true
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -7,7 +7,7 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users, dependent: :destroy
 
   validates :title, presence: true
-  validates :level, numericality: { only_integer: true }
+  validates :level, numericality: { only_integer: true, greater_than_or_equl_to: 0 }
   validates :title, uniqueness: { scope: :level,
                                 message: 'Already exists' }
 
@@ -18,6 +18,10 @@ class Test < ApplicationRecord
   scope :by_category, lambda { |category_name|
     joins(:category)
       .where(categories: { title: category_name })
-      .order(title: :desc).pluck(:title)
+      .order(title: :desc)
   }
+
+  def self.test_list(category_name)
+    by_category(category_name).pluck(:title)
+  end
 end
