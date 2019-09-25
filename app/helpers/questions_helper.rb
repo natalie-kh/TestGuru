9127027ed20223e449f5b4bd982fec6203ef42a5
natalya.bogdanova <nata-mini@mail.ru>
2019-09-25 13:05:08 +0300

    Mistake at question helper

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,6 @@
 module QuestionsHelper
   def header_creator
-    header = @question.persisted? == 'edit' ? 'Edit' : 'Create New'
+    header = @question.persisted? ? 'Edit' : 'Create New'
     "#{header} '#{@question.test.title}' Test Question"
   end
 end
