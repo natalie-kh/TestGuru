dda11f02409a88ce1dd4c7dcfeab06dea2aeb65a
natalya.bogdanova <nata-mini@mail.ru>
2019-09-29 16:15:29 +0300

    Fixes after review

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to cookies[:path] || tests_path
+      redirect_to cookies.delete(:path) || tests_path
     else
       flash.now[:alert] = 'Are You a Guru? Verify your Email and Password please'
       render :new
