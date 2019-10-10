507fd4747d3825cfe486a93e6e1c6693b004eee1
Natalya Bogdanova <nata-minni@mail.ru>
2019-10-10 20:24:42 +1030

    Fixes in js file

--- app/javascript/utilities/password_confirmation.js
+++ app/javascript/utilities/password_confirmation.js
@@ -1,19 +1,19 @@
-document.addEventListener('input', function() {
+document.addEventListener('turbolinks:load', function() {
     var input = document.querySelector('.check_confirmation')
 
     if (input) { input.addEventListener('input', checkDifference ) }
 })
 
 function checkDifference () {
-    var password_conf = document.getElementById('user_password_confirmation')
+    var passwordConf = document.getElementById('user_password_confirmation')
     var password = document.getElementById('user_password')
-    var password_value = password.value
-    var password_conf_value = password_conf.value
+    var passwordValue = password.value
+    var passwordConfValue = passwordConf.value
 
-    if (password_conf_value == '') {
+    if (passwordConfValue == '') {
         this.querySelector('.octicon-check').classList.add('hide')
         this.querySelector('.octicon-x').classList.add('hide')
-    }  else if (password_value == password_conf_value) {
+    }  else if (passwordValue == passwordConfValue) {
         this.querySelector('.octicon-check').classList.remove('hide')
         this.querySelector('.octicon-x').classList.add('hide')
     } else {
