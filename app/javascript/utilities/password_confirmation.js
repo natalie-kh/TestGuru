949e6d67582e5e37ca2721ab2239b78ee5d951ff
natalya.bogdanova <nata-mini@mail.ru>
2019-10-11 16:20:13 +0300

    changed const to var

--- app/javascript/utilities/password_confirmation.js
+++ app/javascript/utilities/password_confirmation.js
@@ -1,14 +1,14 @@
 document.addEventListener('turbolinks:load', function() {
-    const input = document.querySelector('.check_confirmation')
+    var input = document.querySelector('.check_confirmation')
 
     if (input) { input.addEventListener('input', checkDifference ) }
 })
 
 function checkDifference () {
-    const passwordConf = document.getElementById('user_password_confirmation')
-    const password = document.getElementById('user_password')
-    const passwordValue = password.value
-    const passwordConfValue = passwordConf.value
+    var passwordConf = document.getElementById('user_password_confirmation')
+    var password = document.getElementById('user_password')
+    var passwordValue = password.value
+    var passwordConfValue = passwordConf.value
 
     if (passwordConfValue === '') {
         this.querySelector('.octicon-check').classList.add('hide')
