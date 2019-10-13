b0f5f90683edd8cec1d600df7d8216571196d4f6
natalya.bogdanova <nata-mini@mail.ru>
2019-10-13 15:42:23 +0300

    smtp settings -fixes for typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
-  Rails.application.routes.default_url_options[:host] = 'domain.com'
+  Rails.application.routes.default_url_options[:host] = 'shielded-everglades-48459.herokuapp.com''
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'shielded-everglades-48459.herokuapp.com' }
