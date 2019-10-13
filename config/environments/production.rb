f6125db5211bb427c2b0b79ad9aea497ae14a52f
natalya.bogdanova <nata-mini@mail.ru>
2019-10-13 15:30:18 +0300

    smtp settings - typo2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_option = { host: 'shielded-everglades-48459.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'shielded-everglades-48459.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
